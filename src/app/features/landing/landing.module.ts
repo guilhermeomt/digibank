2d522f92e7ceb484c0f97fd1f4346e93a7aa9f20
Guilherme <guilhermet213@gmail.com>
2022-07-23 15:31:34 -0300

    feat: adicionado componente feature card e nova seção na pagina landing

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -4,14 +4,27 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { LandingComponent } from './pages/landing.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { NgIconsModule } from '@ng-icons/core';
-import { FeatherChevronRight } from '@ng-icons/feather-icons';
+import {
+  FeatherChevronRight,
+  FeatherDollarSign,
+  FeatherCreditCard,
+  FeatherBarChart,
+  FeatherRepeat,
+} from '@ng-icons/feather-icons';
+import { FeatureCardComponent } from './components/feature-card/feature-card.component';
 
 @NgModule({
-  declarations: [LandingComponent, HeroComponent],
+  declarations: [LandingComponent, HeroComponent, FeatureCardComponent],
   imports: [
     CommonModule,
     SharedModule,
-    NgIconsModule.withIcons({ FeatherChevronRight }),
+    NgIconsModule.withIcons({
+      FeatherChevronRight,
+      FeatherDollarSign,
+      FeatherCreditCard,
+      FeatherBarChart,
+      FeatherRepeat,
+    }),
   ],
   exports: [LandingComponent],
 })
--- src/app/features/landing/pages/landing.component.ts
+++ src/app/features/landing/pages/landing.component.ts
@@ -2,13 +2,37 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
+  features = [
+    {
+      title: 'Dinheiro rendendo 104% do CDI',
+      iconName: 'feather-dollar-sign',
+      iconColor: '#0CD68A',
+      bgColor: '#C3FFE9',
+    },
+    {
+      title: 'Transfira dinheiro sem pagar taxas',
+      iconName: 'feather-repeat',
+      iconColor: '#0CB1D6',
+      bgColor: '#C3F4FF',
+    },
+    {
+      title: 'Cartão de crédito sem anuidade',
+      iconName: 'feather-credit-card',
+      iconColor: '#B80CD6',
+      bgColor: '#F6C3FF',
+    },
+    {
+      title: 'Acompanhe e controle despesas',
+      iconName: 'feather-bar-chart',
+      iconColor: '#D60C19',
+      bgColor: '#FFC3C7',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

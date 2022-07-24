95fbbf0b2f308c40fcdf3066e61049911a9e98c1
Guilherme <guilhermet213@gmail.com>
2022-07-24 00:03:34 -0300

    feat: adicionado componente footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NgIconsModule } from '@ng-icons/core';
-import { FeatherChevronDown } from '@ng-icons/feather-icons';
+import {
+  FeatherChevronDown,
+  FeatherChevronRight,
+  FeatherFacebook,
+  FeatherInstagram,
+  FeatherYoutube,
+} from '@ng-icons/feather-icons';
 import { ButtonComponent } from './components/button/button.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, ButtonComponent],
-  imports: [CommonModule, NgIconsModule.withIcons({ FeatherChevronDown })],
-  exports: [HeaderComponent, ButtonComponent],
+  declarations: [HeaderComponent, ButtonComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    NgIconsModule.withIcons({
+      FeatherChevronDown,
+      FeatherChevronRight,
+      FeatherFacebook,
+      FeatherInstagram,
+      FeatherYoutube,
+    }),
+  ],
+  exports: [HeaderComponent, ButtonComponent, FooterComponent],
 })
 export class SharedModule {}
